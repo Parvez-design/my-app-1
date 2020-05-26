c806a430b50ad83808f6f31c766dd91f510c6bae
Parvez <shpaah522@gmail.com>
2020-05-26 16:14:59 +0530

    snackbar tutorial

---
+++ app/src/main/java/com/example/myapp1/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.snackbar.Snackbar
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val username = findViewById<EditText>(R.id.username)
+        val passwordAuthentication = findViewById<EditText>(R.id.password)
+        val button = findViewById<Button>(R.id.btn)
+        button.setOnClickListener({
+            val uname = username.text.toString()
+            val passwd = passwordAuthentication.text.toString()
+
+            if (uname.equals("testusername") && passwd.equals("testpassword")) {
+                val snackbar = Snackbar.make(it, "Login Success", Snackbar.LENGTH_LONG)
+                snackbar.setAction("Ok", View.OnClickListener { println("Successfully done") })
+                snackbar.show()
+            } else {
+                val snackbar = Snackbar.make(it, "username/password is wrong", Snackbar.LENGTH_LONG)
+                snackbar.show()
+            }
+        })
+    }
+
+}
+
